93438e3aca78e5bf96ce638ec7a549caeda59fc0
dopeCape <tmank14319@gmail.com>
2025-05-20 01:11:59 +0530

    chroe: nothing

--- src/types/chapter.type.ts
+++ src/types/chapter.type.ts
@@ -8,11 +8,10 @@ import {
 type Chapter = z.infer<typeof chapterSchema>;
 type Quiz = z.infer<typeof quizSchema>;
 type Question = z.infer<typeof questionSchema>
-type AnswerType = z.infer<typeof answerTypeSchema>;
+type AnswerType = z.infer<typeof answerTypeSchema>
 type ModuleContent = z.infer<typeof moduleContentTypeSchema>
 type DifficultyLevel = z.infer<typeof difficultyLevelSchema>
 type Assignment = z.infer<typeof assignmentSchema>
-
 
 export type {
   Chapter, Quiz, Question, AnswerType,
